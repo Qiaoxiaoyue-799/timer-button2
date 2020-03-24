c77604cab3cec375b38040f21e14f490244e046e
Qiaoxiaoyue-799 <1194110992@qq.com>
2020-03-24 17:22:26 +0800

    2

---
+++ timer-button.js
@@ -0,0 +1,54 @@
+var $timerButton =(function(){
+    var $btn = $('<input class="timer-button" type="button" disabled/>'),
+        cfg = {
+            container:'body',
+            num:7,
+            title:'同意'
+        },
+        timer,
+        num;
+    function show(conf){
+        //1.DOM绘制
+        $(cfg.container).append($btn);
+        $.extend(cfg,conf);
+        num = cfg.num;
+        $btn.val(cfg.title+'('+num+'s)');
+        //2.enent bind
+    }
+
+    timer = setInterval(function(){
+        num--;
+        if(num === 0){
+            clearInterval(timer);
+            $btn.val('同意');
+            $btn.removeAttr('disabled');//去掉禁用属性
+        }else{
+            $btn.val(cfg.title+'('+num+'s)');
+        }
+    },1000);
+
+    $btn.click(function(){
+        cfg.onClick();
+    });
+    return{
+        show:show
+    }
+}());
+//不用page load event
+/*封装成对象，有几种方案
+1.全局对象
+var timerBtn={
+    show:function()
+}
+2.工厂函数，一个函数返回值是一个简单对象
+var timerBtn = (function(){
+    return{
+        show:function(){}
+    }
+}())匿名函数
+3.构造函数
+function TimerBtn(){
+
+}
+var tb=new TimerBtn()
+*/
